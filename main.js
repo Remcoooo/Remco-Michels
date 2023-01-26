511bf65a97f85de85227ea4723a41ff890a4cd4a
Remcoooo <91885890+Remcoooo@users.noreply.github.com>
2023-01-26 11:48:54 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -134,60 +134,6 @@ closeWebsite.addEventListener("click", () => {
 });
 
 
-// Data for contact form
-const nameInput = document.querySelector('#name');
-const email = document.querySelector('#email');
-const message = document.querySelector('#message');
-const success = document.querySelector('#success');
-const errorNodes = document.querySelectorAll('.error');
-
-function validateForm() {
-
-  clearMessages();
-  let errorFlag = false;
-
-  if(nameInput.value.length < 1) {
-    errorNodes[0].innerText = "Name is required";
-    nameInput.classList.add("error-border");
-    errorFlag = true;
-  }
-
-  if(!emailIsValid(email.value)) {
-    errorNodes[1].innerText = "Email is invalid";
-    email.classList.add("error-border");
-    errorFlag = true;
-  }
-
-  if(message.value.length < 1) {
-    errorNodes[2].innerText = "Message is required";
-    message.classList.add("error-border");
-    errorFlag = true;
-  }
-
-  if(!errorFlag) {
-    success.innerText = "Message send succesfully";
-  }
-
-}
-
-//Clear error messages
-function clearMessages() {
-  for(let i = 0; i < errorNodes.length; i++) {
-    errorNodes[i].innerText = "";
-  }
-
-  success.innerText = "";
-  nameInput.classList.remove("error-border");
-  email.classList.remove("error-border");
-  message.classList.remove("error-border");
-}
-
-//Check if email is valid
-function emailIsValid(email) {
-  let pattern = /\S+@\S+\.\S+/;
-  return pattern.test(email);
-}
-
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function languageDropdown() {
